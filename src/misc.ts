3913f5cc991555c8009d7e7d20fea0b63a9d7697
Erik Marks <rekmarks@protonmail.com>
2022-03-14 22:31:26 -0700

    Add isNullish, isPlainObject -> isObject

--- src/misc.ts
+++ src/misc.ts
@@ -1,46 +1,6 @@
-export type PlainObject = Record<number | string | symbol, unknown>;
-
-/**
- * A plain object type guard.
- *
- * @param value - The value to check.
- * @returns Whether the specified value is a "plain object", i.e. its runtime
- * type is `object` and its neither `null` nor an `Array`.
- */
-export function isPlainObject(value: unknown): value is PlainObject {
-  return Boolean(value) && typeof value === 'object' && !Array.isArray(value);
-}
-
-/**
- * An alias for {@link Reflect.hasOwnProperty}.
- *
- * @param object - The object to check.
- * @param name - The property name to check for.
- * @returns Whether the specified object has an own property with the specified
- * name.
- */
-export const hasProperty = (
-  object: PlainObject,
-  name: string | number | symbol,
-): boolean => Reflect.hasOwnProperty.call(object, name);
-
-/**
- * Like {@link Array}, but always non-empty.
- *
- * @template T - The non-empty array member type.
- */
-export type NonEmptyArray<T> = [T, ...T[]];
-
-/**
- * A {@link NonEmptyArray} type guard.
- *
- * @template T - The non-empty array member type.
- * @param value - The value to check.
- * @returns Whether the value is a non-empty array.
- */
-export function isNonEmptyArray<T>(value: T[]): value is NonEmptyArray<T> {
-  return Array.isArray(value) && value.length > 0;
-}
+//
+// Types
+//
 
 /**
  * Makes every specified property of the specified object type mutable.
@@ -64,3 +24,69 @@ export type Mutable<
  * @template T - The value that might be present or complete.
  */
 export type Maybe<T> = Partial<T> | null | undefined;
+
+/**
+ * Like {@link Array}, but always non-empty.
+ *
+ * @template T - The non-empty array member type.
+ */
+export type NonEmptyArray<T> = [T, ...T[]];
+
+/**
+ * A JavaScript object that is not `null`, a function, or an array. The object
+ * can still be an instance of a class.
+ */
+export type PlainObject = Record<number | string | symbol, unknown>;
+
+//
+// Type Guards
+//
+
+/**
+ * A {@link NonEmptyArray} type guard.
+ *
+ * @template T - The non-empty array member type.
+ * @param value - The value to check.
+ * @returns Whether the value is a non-empty array.
+ */
+export function isNonEmptyArray<T>(value: T[]): value is NonEmptyArray<T> {
+  return Array.isArray(value) && value.length > 0;
+}
+
+/**
+ * Type guard for "nullishness".
+ *
+ * @param value - A value (literally anything).
+ * @returns `true` if the value is null or undefined, `false` otherwise.
+ */
+export function isNullish(value: unknown): value is null | undefined {
+  return value === null || value === undefined;
+}
+
+/**
+ * A type guard for {@link PlainObject}.
+ *
+ * @param value - The value to check.
+ * @returns Whether the specified value is a "plain object", i.e. its runtime
+ * type is `object` and its neither `null` nor an `Array`.
+ */
+export function isObject(value: unknown): value is PlainObject {
+  return Boolean(value) && typeof value === 'object' && !Array.isArray(value);
+}
+
+//
+// Other utility functions
+//
+
+/**
+ * An alias for {@link Reflect.hasOwnProperty}.
+ *
+ * @param object - The object to check.
+ * @param name - The property name to check for.
+ * @returns Whether the specified object has an own property with the specified
+ * name.
+ */
+export const hasProperty = (
+  object: PlainObject,
+  name: string | number | symbol,
+): boolean => Reflect.hasOwnProperty.call(object, name);
