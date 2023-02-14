69426091961b24c343ecc4661176907ef2a8e3e0
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-02-14 20:53:50 -0300

    Proposed tx data types

--- src/transaction-types.ts
+++ src/transaction-types.ts
@@ -3,71 +3,46 @@ import { Hex } from './hex';
 
 export type Address = Hex | Buffer;
 
+// https://ethereum.org/en/developers/docs/transactions/
 export type Transaction = {
-  /**
-   * The transaction's nonce.
-   */
-  nonce?: bigint | string | number | Buffer;
-
-  /**
-   * The transaction's gas price.
-   */
+  nonce: bigint | string | number | Buffer;
+  from: Address;
+  to: Address;
+  data: Buffer;
   gasPrice?: bigint | string | number | Buffer | null;
-
-  /**
-   * The transaction's gas limit.
-   */
   gasLimit?: bigint | string | number | Buffer;
-
-  /**
-   * The maximum price of the consumed gas
-   * to be included as a tip to the validator
-   */
-  maxPriorityFeePerGas?: bigint | string | number | Buffer;
-
-  /**
-   * the maximum fee per unit of gas willing to be paid for the
-   * transaction (inclusive of _baseFeePerGas_ and _maxPriorityFeePerGas_)
-   */
-  maxFeePerGas?: bigint | string | number | Buffer;
-
-  /**
-   * The transaction's the address is from.
-   */
-  from?: Address;
-
-  /**
-   * The transaction's the address is sent to.
-   */
-  to?: Address;
-
-  /**
-   * The amount of Ether sent.
-   */
   value?: bigint | string | number | Buffer;
-
-  /**
-   * This will contain the data of the message or the init of a contract.
-   */
-  data?: Buffer;
-
-  /**
-   * EC recovery ID.
-   */
   v?: bigint | string | number | Buffer;
-
-  /**
-   * EC signature parameter.
-   */
   r?: bigint | string | number | Buffer;
-
-  /**
-   * EC signature parameter.
-   */
   s?: bigint | string | number | Buffer;
-
-  /**
-   * The transaction type
-   */
   type?: bigint | string | number | Buffer;
 };
+
+// https://eips.ethereum.org/EIPS/eip-2930
+export type EIP2930Tx = {
+  chainId: bigint | string | number | Buffer;
+  accessList: { address: Hex; storageKeys: Hex[] }[];
+} & Transaction;
+
+// https://eips.ethereum.org/EIPS/eip-1559
+export type EIP1559Tx = {
+  gasPrice: never | null;
+  gas: bigint | string | number | Buffer;
+  maxPriorityFeePerGas?: bigint | string | number | Buffer;
+  maxFeePerGas?: bigint | string | number | Buffer;
+} & EIP2930Tx;
+
+// https://eips.ethereum.org/EIPS/eip-4844
+export type EIP4844Tx = {
+  maxFeePerDataGas: bigint | string | number | Buffer;
+  blobVersionedHashes: Hex[] | string[] | Buffer[];
+  blobs?: Buffer[] | Uint8Array[] | number[][] | number[] | bigint[] | Hex[];
+  kzgCommitments?:
+    | Buffer[]
+    | Uint8Array[]
+    | number[][]
+    | number[]
+    | bigint[]
+    | Hex[];
+  kzgProof?: Buffer | Uint8Array | number[] | number | bigint | Hex;
+} & EIP1559Tx;
