a880a3fe7197a9dd0f662bfaf114fe158b8c696e
Elliot Winkler <elliot.winkler@gmail.com>
2023-10-13 16:38:22 -0600

    Add filesystem utils
    This commit adds several utility functions that are related to the
    filesystem in some way.
    
    - Most of the functions here are wrappers around basic filesystem
      operations with better error handling. Node's `fs.promises` module is
      somewhat difficult to work with because [the errors it produces lack
      proper stacktraces][1]. The functions that directly use `fs.promises`
      wrap caught errors in order to supply the missing stacktraces.
    - Two functions make it easier to read and write JSON files (with support
      for JSON-compatible interfaces, such as [JSON5][2]).
    - One function, `createSandbox`, is designed to wrap tests that need
      a temporary directory to work within, such as those for a command-line
      tool that makes changes to the filesystem.
    
    Here are places where we currently use these utilities (or something
    like them):
    
    - https://github.com/MetaMask/action-utils/blob/54ddd730746668cb4c1c88b4edfa720cbecf5e32/src/file-utils.ts
    - https://github.com/MetaMask/create-release-branch/blob/3556dee47163c921186051be7a1f3c98e2049db9/src/fs.ts
    - https://github.com/MetaMask/create-release-branch/blob/3556dee47163c921186051be7a1f3c98e2049db9/tests/helpers.ts
    
    One note about these utilities is that they require Node to use and will
    not work in the browser. Because we already create two kinds of bundles,
    one for CommonJS and another ESM, it would be difficult to create a
    second level of bundles, one for Node and another for the browser.
    Instead of requiring more complexity around the bundle configuration,
    this commit instead introduces another way to import the package.
    
    By default, you'll get all exports that are guaranteed to be
    cross-platform. That means that the file utilities won't show up:
    
    ``` typescript
    // ❌
    import { readFile } from "@metamask/utils";
    ```
    
    If you want all of the cross-platform exports plus the Node-specific
    ones, you will have to import "@metamask/utils/node". For instance:
    
    ``` typescript
    // ✅
    import { readFile } from "@metamask/utils/node";
    ```
    
    [1]: https://github.com/nodejs/node/issues/30944
    [2]: https://www.npmjs.com/package/json5

---
+++ src/fs.ts
@@ -0,0 +1,258 @@
+// This file is intended to be used only in a Node.js context.
+/* eslint-disable import/no-nodejs-modules */
+
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+
+import { isErrorWithCode, wrapError } from './errors';
+import type { Json } from './json';
+
+/**
+ * Information about the file sandbox provided to tests that need temporary
+ * access to the filesystem.
+ */
+export type FileSandbox = {
+  directoryPath: string;
+  withinSandbox: (
+    test: (args: { directoryPath: string }) => Promise<void>,
+  ) => Promise<void>;
+};
+
+/**
+ * Reads the file at the given path, assuming its content is encoded as UTF-8.
+ *
+ * @param filePath - The path to the file.
+ * @returns The content of the file.
+ * @throws An error with a stack trace if reading fails in any way.
+ */
+export async function readFile(filePath: string): Promise<string> {
+  try {
+    return await fs.promises.readFile(filePath, 'utf8');
+  } catch (error) {
+    throw wrapError(error, `Could not read file '${filePath}'`);
+  }
+}
+
+/**
+ * Writes content to the file at the given path.
+ *
+ * @param filePath - The path to the file.
+ * @param content - The new content of the file.
+ * @throws An error with a stack trace if writing fails in any way.
+ */
+export async function writeFile(
+  filePath: string,
+  content: string,
+): Promise<void> {
+  try {
+    await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
+    await fs.promises.writeFile(filePath, content);
+  } catch (error) {
+    throw wrapError(error, `Could not write file '${filePath}'`);
+  }
+}
+
+/**
+ * Reads the assumed JSON file at the given path, attempts to parse it, and
+ * returns the resulting object. Supports a custom parser (in case you want to
+ * use the [JSON5](https://www.npmjs.com/package/json5) package instead).
+ *
+ * @param filePath - The path segments pointing to the JSON file. Will be passed
+ * to path.join().
+ * @param options - Options to this function.
+ * @param options.parser - The parser object to use. Defaults to `JSON`.
+ * @param options.parser.parse - A function that parses JSON data.
+ * @returns The object corresponding to the parsed JSON file, typed against the
+ * struct.
+ * @throws An error with a stack trace if reading fails in any way, or if the
+ * parsed value is not a plain object.
+ */
+export async function readJsonFile<Value extends Json>(
+  filePath: string,
+  {
+    parser = JSON,
+  }: {
+    parser?: { parse: typeof JSON.parse };
+  } = {},
+): Promise<Value> {
+  try {
+    const content = await fs.promises.readFile(filePath, 'utf8');
+    return parser.parse(content);
+  } catch (error) {
+    throw wrapError(error, `Could not read JSON file '${filePath}'`);
+  }
+}
+
+/**
+ * Attempts to write the given JSON-like value to the file at the given path.
+ * Adds a newline to the end of the file. Supports a custom parser (in case you
+ * want to use the [JSON5](https://www.npmjs.com/package/json5) package
+ * instead).
+ *
+ * @param filePath - The path to write the JSON file to, including the file
+ * itself.
+ * @param jsonValue - The JSON-like value to write to the file. Make sure that
+ * JSON.stringify can handle it.
+ * @param options - The options to this function.
+ * @param options.prettify - Whether to format the JSON as it is turned into a
+ * string such that it is broken up into separate lines (using 2 spaces as
+ * indentation).
+ * @param options.stringifier - The stringifier to use. Defaults to `JSON`.
+ * @param options.stringifier.stringify - A function that stringifies JSON.
+ * @returns The object corresponding to the parsed JSON file, typed against the
+ * struct.
+ * @throws An error with a stack trace if writing fails in any way.
+ */
+export async function writeJsonFile(
+  filePath: string,
+  jsonValue: Json,
+  {
+    stringifier = JSON,
+    prettify = false,
+  }: {
+    stringifier?: {
+      stringify: typeof JSON.stringify;
+    };
+    prettify?: boolean;
+  } = {},
+): Promise<void> {
+  try {
+    await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
+    const json = prettify
+      ? stringifier.stringify(jsonValue, null, '  ')
+      : stringifier.stringify(jsonValue);
+    await fs.promises.writeFile(filePath, json);
+  } catch (error) {
+    throw wrapError(error, `Could not write JSON file '${filePath}'`);
+  }
+}
+
+/**
+ * Tests the given path to determine whether it represents a file.
+ *
+ * @param filePath - The path to a (supposed) file on the filesystem.
+ * @returns A promise for true if the file exists or false otherwise.
+ * @throws An error with a stack trace if reading fails in any way.
+ */
+export async function fileExists(filePath: string): Promise<boolean> {
+  try {
+    const stats = await fs.promises.stat(filePath);
+    return stats.isFile();
+  } catch (error) {
+    if (isErrorWithCode(error) && error.code === 'ENOENT') {
+      return false;
+    }
+
+    throw wrapError(error, `Could not determine if file exists '${filePath}'`);
+  }
+}
+
+/**
+ * Tests the given path to determine whether it represents a file.
+ *
+ * @param directoryPath - The path to a (supposed) directory on the filesystem.
+ * @returns A promise for true if the file exists or false otherwise.
+ * @throws An error with a stack trace if reading fails in any way.
+ */
+export async function directoryExists(directoryPath: string): Promise<boolean> {
+  try {
+    const stats = await fs.promises.stat(directoryPath);
+    return stats.isDirectory();
+  } catch (error) {
+    if (isErrorWithCode(error) && error.code === 'ENOENT') {
+      return false;
+    }
+
+    throw wrapError(
+      error,
+      `Could not determine if file exists '${directoryPath}'`,
+    );
+  }
+}
+
+/**
+ * Creates the given directory along with any directories leading up to the
+ * directory. If the directory already exists, this is a no-op.
+ *
+ * @param directoryPath - The path to the desired directory.
+ * @returns What `fs.promises.mkdir` returns.
+ * @throws An error with a stack trace if reading fails in any way.
+ */
+export async function ensureDirectoryStructureExists(
+  directoryPath: string,
+): Promise<string | undefined> {
+  try {
+    return await fs.promises.mkdir(directoryPath, { recursive: true });
+  } catch (error) {
+    throw wrapError(
+      error,
+      `Could not create directory path '${directoryPath}'`,
+    );
+  }
+}
+
+/**
+ * Removes the given file or directory, if it exists.
+ *
+ * @param entryPath - The path to the file or directory.
+ * @returns What `fs.promises.rm` returns.
+ * @throws An error with a stack trace if removal fails in any way.
+ */
+export async function forceRemove(entryPath: string): Promise<void> {
+  try {
+    return await fs.promises.rm(entryPath, {
+      recursive: true,
+      force: true,
+    });
+  } catch (error) {
+    throw wrapError(error, `Could not remove file or directory '${entryPath}'`);
+  }
+}
+
+/**
+ * Constructs a sandbox object which can be used in tests that need temporary
+ * access to the filesystem.
+ *
+ * @param projectName - The name of the project.
+ * @returns The sandbox object. This contains a `withinSandbox` function which
+ * can be used in tests (see example).
+ * @example
+ * ```typescript
+ * const { withinSandbox } = createSandbox('utils');
+ *
+ * // ... later ...
+ *
+ * it('does something with the filesystem', async () => {
+ *   await withinSandbox(async ({ directoryPath }) => {
+ *     await fs.promises.writeFile(
+ *       path.join(directoryPath, 'some-file'),
+ *       'some content',
+ *       'utf8'
+ *     );
+ *   })
+ * });
+ * ```
+ */
+export function createSandbox(projectName: string): FileSandbox {
+  const directoryPath = path.join(os.tmpdir(), projectName);
+
+  return {
+    directoryPath,
+    async withinSandbox(
+      test: (args: { directoryPath: string }) => Promise<void>,
+    ) {
+      if (await directoryExists(directoryPath)) {
+        throw new Error(`${directoryPath} already exists, cannot continue`);
+      }
+
+      await ensureDirectoryStructureExists(directoryPath);
+
+      try {
+        await test({ directoryPath });
+      } finally {
+        await forceRemove(directoryPath);
+      }
+    },
+  };
+}
