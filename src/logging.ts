41e0285cf7c5fedf673493ea7f420d70dfbaeafd
Elliot Winkler <elliot.winkler@gmail.com>
2024-06-21 14:11:51 -0600

    WIP - Add levels to logging

--- src/logging.ts
+++ src/logging.ts
@@ -1,7 +1,16 @@
+/*
 import type { Debugger } from 'debug';
 import debug from 'debug';
+*/
 
+import loglevel, { getLogger } from 'loglevel';
+import type { Logger } from 'loglevel';
+import loglevelPrefixPlugin from 'loglevel-plugin-prefix';
+// import loglevelDebug from 'loglevel-debug';
+
+/*
 const globalLogger = debug('metamask');
+*/
 
 /**
  * Creates a logger via the `debug` library whose log messages will be tagged
@@ -15,9 +24,11 @@ const globalLogger = debug('metamask');
  * your NPM package if you're developing one.
  * @returns An instance of `debug`.
  */
+/*
 export function createProjectLogger(projectName: string): Debugger {
   return globalLogger.extend(projectName);
 }
+*/
 
 /**
  * Creates a logger via the `debug` library which is derived from the logger for
@@ -33,9 +44,60 @@ export function createProjectLogger(projectName: string): Debugger {
  * file where you're using this logger or some other name.
  * @returns An instance of `debug`.
  */
+/*
 export function createModuleLogger(
   projectLogger: Debugger,
   moduleName: string,
 ): Debugger {
   return projectLogger.extend(moduleName);
+}
+*/
+
+loglevelPrefixPlugin.reg(loglevel);
+
+/**
+ * Constructs a `loglevel` logger which prefixes its messages using the given
+ * `format` function.
+ *
+ * @param name - The name assigned to the logger.
+ * @param options - The options.
+ * @param options.prefix - The prefix to apply to each log message.
+ * @returns The logger.
+ */
+export function createLogger(
+  name: string,
+  { prefix = '' }: { prefix?: string } = {},
+): Logger {
+  const logger = getLogger(name);
+
+  // loglevelDebug(logger);
+
+  loglevelPrefixPlugin.apply(logger, {
+    format(level, loggerName, timestamp) {
+      return `${timestamp.toISOString()} ${String(
+        loggerName,
+      )} ${level.toUpperCase()}${prefix}`;
+    },
+  });
+
+  return logger;
+}
+
+/**
+ *
+ */
+function createNullLogger() {
+  const logger: Logger = getLogger('null');
+  for (const methodName of [
+    'trace',
+    'debug',
+    'info',
+    'warn',
+    'error',
+  ] as const) {
+    logger[methodName] = () => {
+      // do nothing
+    };
+  }
+  return logger;
 }
