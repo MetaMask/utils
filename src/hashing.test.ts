85e2b843c7191ed2affc5c403252ef3d8285d40c
Frederik Bolding <frederik.bolding@gmail.com>
2025-12-17 11:27:38 +0100

    Simplify

--- src/hashing.test.ts
+++ src/hashing.test.ts
@@ -34,14 +34,10 @@ describe('sha256', () => {
   it('falls back to noble when digest function is unavailable', async () => {
     const nobleSpy = jest.spyOn(nobleHashes, 'sha256');
 
-    // The global does not exist in Node 18, so we cannot remove it.
-    // eslint-disable-next-line jest/no-if
-    if (!isNode18) {
-      Object.defineProperty(globalThis.crypto.subtle, 'digest', {
-        value: undefined,
-        writable: true,
-      });
-    }
+    Object.defineProperty(globalThis.crypto.subtle, 'digest', {
+      value: undefined,
+      writable: true,
+    });
 
     const digest = await sha256(stringToBytes('foo bar'));
     expect(bytesToHex(digest)).toBe(
@@ -54,14 +50,10 @@ describe('sha256', () => {
   it('falls back to noble when subtle APIs are unavailable', async () => {
     const nobleSpy = jest.spyOn(nobleHashes, 'sha256');
 
-    // The global does not exist in Node 18, so we cannot remove it.
-    // eslint-disable-next-line jest/no-if
-    if (!isNode18) {
-      Object.defineProperty(globalThis.crypto, 'subtle', {
-        value: undefined,
-        writable: true,
-      });
-    }
+    Object.defineProperty(globalThis.crypto, 'subtle', {
+      value: undefined,
+      writable: true,
+    });
 
     const digest = await sha256(stringToBytes('foo bar'));
     expect(bytesToHex(digest)).toBe(
