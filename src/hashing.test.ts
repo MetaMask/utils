47a84733d56714bc0eaaa097d33076c25092c4c1
Frederik Bolding <frederik.bolding@gmail.com>
2025-12-17 10:31:24 +0100

    Fix lint

--- src/hashing.test.ts
+++ src/hashing.test.ts
@@ -1,21 +1,24 @@
 import * as nobleHashes from '@noble/hashes/sha256';
-import { bytesToHex, stringToBytes } from "./bytes"
-import { sha256 } from "./hashing"
+
+import { bytesToHex, stringToBytes } from './bytes';
+import { sha256 } from './hashing';
 
 describe('sha256', () => {
-    it('returns a digest for a byte array', async () => {
-        const digest = await sha256(stringToBytes("foo bar"));
-        const hex = bytesToHex(digest);
-        expect(hex).toStrictEqual('0xfbc1a9f858ea9e177916964bd88c3d37b91a1e84412765e29950777f265c4b75');
-    });
+  it('returns a digest for a byte array', async () => {
+    const digest = await sha256(stringToBytes('foo bar'));
+    expect(bytesToHex(digest)).toBe(
+      '0xfbc1a9f858ea9e177916964bd88c3d37b91a1e84412765e29950777f265c4b75',
+    );
+  });
 
-    it('returns a digest for a larger byte array', async () => {
-        const digest = await sha256(new Uint8Array(1024).fill(1));
-        const hex = bytesToHex(digest);
-        expect(hex).toStrictEqual('0x5a648d8015900d89664e00e125df179636301a2d8fa191c1aa2bd9358ea53a69');
-    });
+  it('returns a digest for a larger byte array', async () => {
+    const digest = await sha256(new Uint8Array(1024).fill(1));
+    expect(bytesToHex(digest)).toBe(
+      '0x5a648d8015900d89664e00e125df179636301a2d8fa191c1aa2bd9358ea53a69',
+    );
+  });
 
-it('falls back to noble when digest function is unavailable', async () => {
+  it('falls back to noble when digest function is unavailable', async () => {
     const nobleSpy = jest.spyOn(nobleHashes, 'sha256');
 
     Object.defineProperty(globalThis.crypto.subtle, 'digest', {
@@ -23,9 +26,10 @@ it('falls back to noble when digest function is unavailable', async () => {
       writable: true,
     });
 
-            const digest = await sha256(stringToBytes("foo bar"));
-        const hex = bytesToHex(digest);
-        expect(hex).toStrictEqual('0xfbc1a9f858ea9e177916964bd88c3d37b91a1e84412765e29950777f265c4b75');
+    const digest = await sha256(stringToBytes('foo bar'));
+    expect(bytesToHex(digest)).toBe(
+      '0xfbc1a9f858ea9e177916964bd88c3d37b91a1e84412765e29950777f265c4b75',
+    );
 
     expect(nobleSpy).toHaveBeenCalled();
   });
@@ -38,10 +42,11 @@ it('falls back to noble when digest function is unavailable', async () => {
       writable: true,
     });
 
-            const digest = await sha256(stringToBytes("foo bar"));
-        const hex = bytesToHex(digest);
-        expect(hex).toStrictEqual('0xfbc1a9f858ea9e177916964bd88c3d37b91a1e84412765e29950777f265c4b75');
+    const digest = await sha256(stringToBytes('foo bar'));
+    expect(bytesToHex(digest)).toBe(
+      '0xfbc1a9f858ea9e177916964bd88c3d37b91a1e84412765e29950777f265c4b75',
+    );
 
     expect(nobleSpy).toHaveBeenCalled();
   });
-})
+});
