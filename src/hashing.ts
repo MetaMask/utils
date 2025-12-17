8961a54da11d0bcae6d9a5c7e8ea0e3e6a31b068
Frederik Bolding <frederik.bolding@gmail.com>
2025-12-17 13:37:51 +0100

    Fix lint

--- src/hashing.ts
+++ src/hashing.ts
@@ -17,7 +17,9 @@ export async function sha256(bytes: Uint8Array): Promise<Uint8Array> {
     globalThis.crypto.subtle?.digest
   ) {
     // eslint-disable-next-line no-restricted-globals
-    return new Uint8Array(await globalThis.crypto.subtle.digest('SHA-256', bytes));
+    return new Uint8Array(
+      await globalThis.crypto.subtle.digest('SHA-256', bytes),
+    );
   }
   return nobleSha256(bytes);
 }
